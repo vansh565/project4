35fc51053ceb43011bff4b0f79773e3f79821e50
vansh565 <sharmavansh969@gmail.com>
2024-09-28 15:06:26 +0530

    Add files via upload

---
+++ userhome.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+    <head>
+       
+        
+            <title>www.shree.com</title>
+            <link rel="stylesheet" type="text/css" href="new.css">
+             <link rel="icon" type="image/jpg" href="4.jpg">
+          
+            <div class="main">
+
+          
+        </head> 
+        <body> 
+<div class="main">
+        <div class="navbar">
+
+        <div class="icon">
+            <h2 class="logo">NotEsIn</h2>
+        </div>
+          <div class="menu">
+                <ul>
+                  
+                   
+            <li><a id="v" href ="./2nd.html">HOME</a></li>
+           <li><a  id="v" href ="">ABOUT</a></li>
+            <li><a  id="v"href ="">CONTACT</a></li>
+           <li><a  id="v" href =""> SERVICES</a></li>\
+           <li><a  id="v" href ="">TEACHERS</a></li>
+           
+            </nav></ul>
+          </div>
+          <div class="banner-area">
+            
+          </div>
+          <div class="search">
+            <input class="srch" type="search"name=""placeholder="type to text">
+          <a href="https://www.google.com/search"><button class="btn">search</button></a>   
+          </div>
+            
+           
+            
+        </div>
+        
+                    </form><br><br>
+                
+<div class=" content">
+    <h1> SHREE <br><span> INSTITUTE OF</span> <br> EDUCATION</h1>
+       <h2> <p class="par">
+       <h2> NO.1 Institution of Jammu</h2><br>
+        
+           <h2> <br> "A Step Towards Success"</p></h2>
+            <button class="cn"><a href="login.html">LOGIN</a></button>
+          <br>
+        <h2>or</h2>    <button class="cn"><a href="signup.html">SIGNUP</a></button>
+        </body>
+</html>
